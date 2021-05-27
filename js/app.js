a4581d03a6aac81eb22763d6fe9d236dab983ca4
VirginieLemaire <virginielemaire@free.fr>
2021-05-27 21:05:56 +0200

    jouer et case d'arrivée

--- js/app.js
+++ js/app.js
@@ -1,29 +1,36 @@
 var app = {};
-  app.player = [{ //le joueur
+  app.player = { //le joueur
     //gérer la position du joueur sur la grille
     x: 0, 
     y: 0,
     direction: "right",
-  }]
-  app.targetCell = [{ //case à atteindre
+  }
+  app.targetCell = { //case à atteindre
     x: 5,
     y: 3,
-  }]
+  }
   app.drawBoard= function(){
     nbRows = 4;
     nbCells = 6;
     for (var i=0; i<nbRows; i++) {
-      var row =document.createElement('div');
+      var row = document.createElement('div');
       row.className ='row';
       document.querySelector('#board').appendChild(row);
       for (var j=0; j<nbCells; j++) {
-        var cell =document.createElement('div');
-        cell.className ='cell';
+        var cell = document.createElement('div');
+        cell.className = 'cell';
         row.appendChild(cell);
+        if (j === app.targetCell.x && j === app.targetCell.y) {
+          cell.classList.toggle('targetCell')
+        } else if (j === app.player.x && i === app.player.y) {
+          var playerIn = document.createElement('div');
+          playerIn.className = 'player ';
+          cell.appendChild(playerIn);
+        } 
       }
     }
   }
   app.init= function () {
     console.log(this.drawBoard());
   }
-  app.init();
+app.init();
