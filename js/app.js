699e33a2a3006b087a0fdb6863b2289de1ae4915
VirginieLemaire <virginielemaire@free.fr>
2021-05-27 17:17:56 +0200

    prépa terrain ok

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
-var app = {
+var app = {};
+  app.player = [{ //le joueur
+    //gérer la position du joueur sur la grille
+    x: 0; 
+    y: 0;
+    direction: "right";
+  }]
+
+  app.targetCell = [{ //case à atteindre
+    x: 5;
+    y: 3;
+  }]
+
+
+
   init: function () {
     console.log('init !');
   }
-};
+
 
 document.addEventListener('DOMContentLoaded', app.init);
