9f7c30d7e617005927e6209bea377938e60daf7a
VirginieLemaire <virginielemaire@free.fr>
2021-05-27 18:07:49 +0200

    dessin grille ok -sans CSS

--- js/app.js
+++ js/app.js
@@ -1,21 +1,29 @@
 var app = {};
   app.player = [{ //le joueur
     //gérer la position du joueur sur la grille
-    x: 0; 
-    y: 0;
-    direction: "right";
+    x: 0, 
+    y: 0,
+    direction: "right",
   }]
-
   app.targetCell = [{ //case à atteindre
-    x: 5;
-    y: 3;
+    x: 5,
+    y: 3,
   }]
-
-
-
-  init: function () {
-    console.log('init !');
+  app.drawBoard= function(){
+    nbRows = 4;
+    nbCells = 6;
+    for (var i=0; i<nbRows; i++) {
+      var row =document.createElement('div');
+      row.className ='row';
+      document.querySelector('#board').appendChild(row);
+      for (var j=0; j<nbCells; j++) {
+        var cell =document.createElement('div');
+        cell.className ='cell';
+        row.appendChild(cell);
+      }
+    }
   }
-
-
-document.addEventListener('DOMContentLoaded', app.init);
+  app.init= function () {
+    console.log(this.drawBoard());
+  }
+  app.init();
